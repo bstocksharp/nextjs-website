2d8dbc419c1a9fecea1588305f8f51941a0131bf
bstocksharp <bstocksharp@gmail.com>
2024-01-30 16:40:41 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By VAL SMELLS
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
