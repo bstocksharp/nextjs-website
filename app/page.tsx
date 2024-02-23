1fd19a5e170aa27c7a70835a3768596d3057cd17
bstocksharp <bstocksharp@gmail.com>
2024-02-23 17:53:06 -0600

    fancy front page

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,53 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
-  const [myTitle, setMyTitle] = useState("Software Engineer.");
+  const [myTitle, setMyTitle] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    const timeouts = [
-      setTimeout(() => setMyTitle("UI/UX Designer."), 4000),
-      setTimeout(() => setMyTitle("Software Engineer."), 8000),
-      // If you want the third thing to stay permanently
-      // you don't need to set another timeout
-    ];
+    const titles = ["Software Engineer.", "UI/UX Designer.", "Father."]; // Add more titles as needed
+    let timeoutId: string | number | NodeJS.Timeout | undefined;
 
-    return () => {
-      timeouts.forEach(clearTimeout);
+    const typeWriter = (index: number) => {
+      if (index < titles.length) {
+        const title = titles[index];
+        for (let i = 1; i <= title.length; i++) {
+          timeoutId = setTimeout(() => {
+            setMyTitle(title.substring(0, i));
+          }, i * 200); // Adjust the interval duration as needed
+        }
+        setTimeout(() => eraseText(index), title.length * 200 + 1000); // 1000 milliseconds delay before erasing
+      }
     };
-  }, []);
+
+    const eraseText = (index: number) => {
+      if (index < titles.length - 1) {
+        setMyTitle("");
+        setTimeout(() => setCurrentIndex(index + 1), 1000);
+      }
+    };
+
+    typeWriter(currentIndex); // Start typing with the first title
+
+    return () => clearTimeout(timeoutId);
+  }, [currentIndex]);
 
   return (
     <div className="home-wrapper">
       <div className="pic-and-words-wrapper">
         <div className="words-wrapper">
           <h1>Hi! I&apos;m Bryce. </h1>
-          <h2>I&apos;m a </h2>
-          <h2 className="title-box-wrapper">{myTitle}</h2>
+          <div className="words-single-line">
+            <h2>I&apos;m a </h2>
+            <h2 className="title-box-wrapper">{myTitle}</h2>
+          </div>
         </div>
         <div className="profile-image">
           <Image
             src="/profile_pic_2_removed.png"
             alt="Profile Picture"
-            width={400}
-            height={400}
+            width={350}
+            height={350}
             style={{ borderRadius: "100%" }}
           />
         </div>
